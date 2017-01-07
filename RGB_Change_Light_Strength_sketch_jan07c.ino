3e36f8237dda908861ceebfb34249277ce24a3c4
Brave Tea <Alexvthooft@gmail.com>
2017-01-07 22:48:46 +0100

    Add files via upload
    Source File - Original Code

---
+++ RGB_Change_Light_Strength_sketch_jan07c.ino
@@ -0,0 +1,44 @@
+#define RED 3
+#define GREEN 5
+#define BLUE 6
+
+int photoPin = 0;
+int lightValue = analogRead(photoPin);
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+  digitalWrite(RED, HIGH);
+  digitalWrite(GREEN, HIGH);
+  digitalWrite(BLUE, HIGH);
+}
+
+void loop()
+{
+  int lightValue = analogRead(photoPin);
+  Serial.println(lightValue);
+  delay(1);
+  if (lightValue < 500)
+  {
+    analogWrite(RED, 255);
+    analogWrite(GREEN, 0);
+    analogWrite(BLUE, 0);
+  }
+  if (lightValue > 500 && lightValue < 700)
+  {
+    analogWrite(RED, 0);
+    analogWrite(GREEN, 0);
+    analogWrite(BLUE, 255);
+  }
+  if (lightValue > 700)
+  {
+    analogWrite(RED, 0);
+    analogWrite(GREEN, 255);
+    analogWrite(BLUE, 0);
+  }
+  delay(1);
+}
+
